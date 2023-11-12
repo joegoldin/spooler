d0bb0740534b999ee2902b6e8f329aa3770dbf18
Joe Goldin <joe@joegold.in>
2023-11-12 13:01:05 -0800

    Revert "aider: Implemented the feature where the edit button makes the row editable and changes to a save button."
    This reverts commit d268e94c2a338d06a32bb6c48b8a8555df0a6810.

--- frontend/spooler/src/components/SpoolList.js
+++ frontend/spooler/src/components/SpoolList.js
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
 import { FaArrowUp, FaArrowDown, FaArchive } from "react-icons/fa";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import "./SpoolList.css";
+import "./SpoolList.css"; // Make sure to create this CSS file for animations
 
 function SpoolList({ title, onEdit, onDelete, onSort, onArchive, spools }) {
-  const [editingHistoryId, setEditingHistoryId] = useState(null);
-  const [editedHistoryEntry, setEditedHistoryEntry] = useState({ used_amount: '', note: '' });
   const [editingHistoryId, setEditingHistoryId] = useState(null);
   const [editedHistoryEntry, setEditedHistoryEntry] = useState({ used_amount: '', note: '' });
 
@@ -15,11 +12,12 @@ function SpoolList({ title, onEdit, onDelete, onSort, onArchive, spools }) {
   };
 
   const handleSaveHistory = (spoolId) => {
-    // Assuming onEditHistory is a prop passed down to handle the API call
-    const handleSaveHistory = (spoolId) => {
-      onEditHistory(spoolId, editedHistoryEntry);
-      setEditingHistoryId(null);
-    };
+    // Here you would call the API to save the edited history entry
+    // After saving, you would clear the editing state and refresh the spool list
+    console.log('Save history entry:', spoolId, editedHistoryEntry);
+    setEditingHistoryId(null);
+    // Call the API to save the changes, then refresh the spool list
+    // onEditHistory(spoolId, editedHistoryEntry);
   };
 
   const handleHistoryInputChange = (field, value) => {
@@ -146,44 +144,15 @@ function SpoolList({ title, onEdit, onDelete, onSort, onArchive, spools }) {
                           {spool.usage_history.map((historyEntry) => (
                             <tr key={historyEntry.id}>
                               <td>{new Date(historyEntry.timestamp).toLocaleDateString()} {new Date(historyEntry.timestamp).toLocaleTimeString()}</td>
+                              <td>{historyEntry.used_amount}g</td>
+                              <td>{historyEntry.note}</td>
                               <td>
-                                {editingHistoryId === historyEntry.id ? (
-                                  <input
-                                    type="number"
-                                    value={editedHistoryEntry.used_amount}
-                                    onChange={(e) => handleHistoryInputChange('used_amount', e.target.value)}
-                                  />
-                                ) : (
-                                  `${historyEntry.used_amount}g`
-                                )}
-                              </td>
-                              <td>
-                                {editingHistoryId === historyEntry.id ? (
-                                  <input
-                                    type="text"
-                                    value={editedHistoryEntry.note}
-                                    onChange={(e) => handleHistoryInputChange('note', e.target.value)}
-                                  />
-                                ) : (
-                                  historyEntry.note
-                                )}
-                              </td>
-                              <td>
-                                {editingHistoryId === historyEntry.id ? (
-                                  <button
-                                    onClick={() => handleSaveHistory(spool.id)}
-                                    className="btn btn-sm btn-success mx-1"
-                                  >
-                                    Save
-                                  </button>
-                                ) : (
-                                  <button
-                                    onClick={() => handleEditHistoryClick(spool.id, historyEntry)}
-                                    className="btn btn-sm btn-warning mx-1"
-                                  >
-                                    Edit
-                                  </button>
-                                )}
+                                <button
+                                  onClick={() => handleEditHistory(spool.id, historyEntry)}
+                                  className="btn btn-sm btn-warning mx-1"
+                                >
+                                  Edit
+                                </button>
                                 <button
                                   onClick={() => handleDeleteHistory(spool.id, historyEntry.id)}
                                   className="btn btn-sm btn-danger mx-1"
